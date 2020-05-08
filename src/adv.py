5cb0fb87f1163748913b5a814931fbe43c5bb922
Asher Kobin <Asher@Ashers-MBP.fios-router.home>
2020-05-07 20:05:13 -0400

    working on inventory

--- src/adv.py
+++ src/adv.py
@@ -102,7 +102,11 @@ Room Commands:
 Item Commands:
 - pickup [item_name] (pickup an item that you have found)
 - drop [item_name] (drop an item from your inventory)
-- inspect [item_name] (inspect an item in your inventory)""")
+
+Inventory Commands:
+- i (displays your inventory)
+- inspect [item_name] (inspect an item)
+- use [item_name] (use an item in your)""")
 
 def search():
   cprint("\nYou search the area and see...", "white")
@@ -114,6 +118,9 @@ def search():
 
 def look():
   print_room_description()
+
+def pickup(item):
+  print("you picked up " + item)
 
 # def move_to(dir, cur_loc):
 #   attribute = dir + "_to"
@@ -131,7 +138,8 @@ options = {
   "w": west,
   "?": list_commands,
   "search": search,
-  "look": look
+  "look": look,
+  "pickup": pickup
 }
 
 cash_needed_to_win = 10000
@@ -153,14 +161,23 @@ cprint("\nType '?' to list player commands")
 cprint(f"\nYou are in the {player.room.name}", "green") 
 cprint(player.room.description, "white")
 
-while (True):  
+while (True):
   action = input("\nAction: ")
 
-  if (action == "q"):
+  decontructed_command = action.split(" ")
+
+  if (len(decontructed_command) > 1):
+    verb = decontructed_command[0]
+    subject = decontructed_command[1]
+  else:
+    verb = decontructed_command[0]
+    subject = None
+
+  if (verb == "q"):
     break
 
   try:
-    options[action]()
+    options[verb](subject)
   except KeyError:
     print("\nInvalid Action: Type ? to get list of actions.")
 
